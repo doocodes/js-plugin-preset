5351431b390eab3ae23c7d8f326bea843e034125
doocodes <milosh.djordjevic@gmail.com>
2017-04-07 15:39:05 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,43 @@
+/*!
+ * jQuery preset plugin
+ * Author: Docodes <milos@themelaboratory.com>
+ * Licensed under the MIT license
+ */
+;(function ($, window, document, undefined) {
+    'use strict';
+
+    $.fn.plugin = function () {
+
+        // Check if namespace has already been initialized
+        if (!$.namespace) {
+            $.namespace = {};
+        };
+
+
+        /**
+         * Real magic happends here :D
+         * @param elements
+         * @param options
+         */
+        $.namespace.plugin = function (elements, options) {
+
+
+        };
+
+        // Extend the $.fn object
+        $.fn.namespace_plugin = function (options) {
+
+            // Merge the passed in options with the default options
+            options = $.extend({}, $.fn.plugin.options, options);
+
+            return this.each(function () {
+                (new $.namespace.plugin(this, options));
+            });
+        };
+    };
+
+    // Default options specified by the developer
+    $.fn.plugin.options = {
+
+    };
+})(jQuery, window, document);
